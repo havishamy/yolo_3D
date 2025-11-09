971394deddde3813ae6147d05ac3172de393cc96
havishamy <dsj0620@mail.ustc.edu.cn>
2025-11-09 08:42:00 +0000

    update

--- train.py
+++ train.py
@@ -1,20 +1,32 @@
 from ultralytics import YOLO
 
+def count_parameters_pytorch(model):
+    """计算PyTorch模型的参数量"""
+    total_params = sum(p.numel() for p in model.parameters())
+    trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    
+    print(f"总参数量: {total_params:,}")
+    print(f"可训练参数量: {trainable_params:,}")
+    print(f"不可训练参数量: {total_params - trainable_params:,}")
+    
+    return total_params, trainable_params
+
 def main():
     # 1. 加载模型（选择合适的规模：n/s/m/l/x）
     # 这里用 YOLOv11n（速度快，轻量），你也可以换成 'yolov11s.pt' 等
-    model = YOLO("/home/dsj/code/ultralytics/apply/yolo11n.pt")
+    model = YOLO("/home/dsj/code/ultralytics/apply/ultralytics/cfg/models/11/yolo11-seg.yaml")
+    count_parameters_pytorch(model)
 
     # 2. 开始训练
     results = model.train(
         data="/home/dsj/code/ultralytics/ultralytics/cfg/datasets/CLAD.yaml",   # 数据集配置文件路径
         epochs=500,                 # 训练轮数
-        batch=16,                   # batch size，根据显存调整
+        batch=4,                   # batch size，根据显存调整
         imgsz=640,                  # 输入图片大小
         workers=4,                  # 数据加载线程数
-        device=[4,5,6,7],      # 训练设备，0 表示第一张 GPU
+        device=0,      # 训练设备，0 表示第一张 GPU
         project="/home/dsj/code/ultralytics/apply/ultralytics/runs/train",       # 保存日志的目录
-        name="yolov11_glassware_1",   # 实验名字
+        name="yolov11_0timetest",   # 实验名字
         pretrained=True,            # 使用预训练权重
         optimizer="SGD",            # 也可以选 Adam/AdamW
         lr0=0.01,                   # 初始学习率
